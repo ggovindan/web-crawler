10af79702f864596cca0a75c04673748037f2d75
ggovindan <gxguru@gmail.com>
2017-01-28 00:49:13 -0800

    Changed from synchronous call to requests to aiohttp ClientSessions

--- web_crawler.py
+++ web_crawler.py
@@ -4,62 +4,107 @@ import asyncio
 import re
 from bs4 import BeautifulSoup
 import requests
+from aiohttp import ClientSession
+import signal
 
-base_url = 'http://www.bbc.com'
-MAX_LEVEL = 5
-MAX_LINKS = 1
-current_level = 0
-link_match = []
-base_url_pattern = r'\b(http[s]*://w{0,3}.{0,1}[a-zA-Z0-9-_]+.[a-zA-Z]+)'
-external_url_pattern = r'\b(http[s]*://w{0,3}.{0,1})'
 
-def shutdown():
-  print("shutting down all tasks")
-  for task in asyncio.Task.all_tasks():
-    task.cancel()
+class WebCrawler:
 
-async def crawl(url_to_parse, keyword):
-  if len(link_match) >= MAX_LINKS:
-    shutdown()
-  html_page = requests.get(url_to_parse)
-  bs_content = BeautifulSoup(html_page.content, 'html.parser')
-  tasks = []
-  
-  # first find if any of the links in the current page have the keyword
-  links = bs_content.find_all('a')
-  for link in links:
-    if link.text.lower().find(keyword.lower()) > 0:
-      print("match={}".format(link.text))
-      print("link={}".format(link.attrs['href']))
-      link_match.append(link)
+    def __init__(self, base_url='http://www.thehindu.com'):
+        self.base_url = base_url
+        self.MAX_LEVEL = 5
+        self.MAX_LINKS = 5
+        self.link_match = []
+        self.current_level = 0
+        self.base_url_pattern = r'\b(http[s]*://w{0,3}.{0,1}[a-zA-Z0-9-_]+.[a-zA-Z]+)'
+        self.external_url_pattern = r'\b(http[s]*://w{0,3}.{0,1})'
+        self.complete_url_pattern = r'\b(http[s]*://w{0,3}.{0,1}[a-zA-Z0-9-_]+.[a-zA-Z]+[/a-zA-Z0-9-_]*)'
 
-  if len(link_match) < 5: #or current_level < MAX_LEVEL:
-    #find the sublinks of this base_url from links and call crawls asynchronously on them
-    for link in links:
-      if link.text.strip(' ') != '':
-        if len(re.findall(base_url_pattern, link.attrs.get('href'))): 
-          #its an full internal link
-          print("FULL_LINK: {}".format(link.attrs['href']))
-          task = asyncio.ensure_future(crawl(link.attrs['href'], keyword))
-          tasks.append(task)
-        elif len(re.findall(external_url_pattern, link.attrs.get('href'))) == 0:
-          #its an internal sublink without the base_url
-          if link.attrs.get('href').find('javascript') > -1:
-            continue
-          url = base_url.rstrip('/') + link.attrs['href']
-          print("INTERNAL_LINK: {}".format(url))
-          task = asyncio.ensure_future(crawl(url, keyword))
-          tasks.append(task)
+    def shutdown(self):
+        print("shutting down all tasks")
+        for task in asyncio.Task.all_tasks():
+            task.cancel()
+
+    def spawn_tasks(self, links, keyword):
+        tasks = []
+        for link in links:
+            try:
+                if link.text.strip(' ') != '':
+                    url_match = re.findall(
+                        self.base_url_pattern, link.attrs['href'])
+                    if len(url_match) and url_match[0] == self.base_url:
+                        # its an full internal link
+                        print("FULL_LINK: {}".format(link.attrs['href']))
+                        task = asyncio.ensure_future(
+                            self.crawl(link.attrs['href'], keyword))
+                        tasks.append(task)
+                    elif len(re.findall(self.external_url_pattern, link.attrs.get('href'))) == 0:
+                        # its an internal sublink without the base_url
+                        if link.attrs.get('href').find('javascript') > -1:
+                            # avoid any javascript(void)
+                            continue
+                        url = self.base_url.rstrip('/') + link.attrs['href']
+                        print("INTERNAL_LINK: {}".format(url))
+                        task = asyncio.ensure_future(self.crawl(url, keyword))
+                        tasks.append(task)
+                    else:
+                        # its an external link, could be an advertisement
+                        # website. ignore it
+                        continue
+            except KeyError:
+                print("got a key error link={}".format(link))
+                # href was not found.. lets parse the link object to retrieve
+                # any url
+                url_match = re.findall(self.complete_url_pattern, str(link))
+                if len(url_match) and url_match[0].find(self.base_url) == 0:
+                    print("EXTRACTED_LINK: {}".format(url_match[0]))
+                    task = asyncio.ensure_future(
+                        self.crawl(url_match[0], keyword))
+                    tasks.append(task)
+                continue
+        return tasks
+
+    async def crawl(self, url_to_parse, keyword='tiruchirapalli'):
+        if len(self.link_match) >= self.MAX_LINKS:
+            self.shutdown()
+        if self.current_level >= self.MAX_LEVEL:
+            print(
+                "max level reached current_level={}".format(
+                    self.current_level))
+            self.shutdown()
+
+        async with ClientSession() as session:
+            async with session.get(url_to_parse) as response:
+                content = await response.text()
+                bs_content = BeautifulSoup(content, 'html.parser')
+
+        # first find if any of the links in the current page have the keyword
+        links = bs_content.find_all('a')
+        for link in links:
+            if link.text.lower().find(keyword.lower()) > 0:
+                with open('links.txt', 'w+') as file:
+                    file.write("match={}".format(link.text))
+                    file.write("link={}".format(link.attrs['href']))
+                print("match={}".format(link.text))
+                print("link={}".format(link.attrs['href']))
+                self.link_match.append(link)
+
+        # or current_level < MAX_LEVEL:
+        if len(self.link_match) < self.MAX_LINKS:
+            self.current_level += 1
+            # find the sublinks of this base_url from links and call crawls
+            # asynchronously on them
+            tasks = self.spawn_tasks(links, keyword)
+            results = asyncio.gather(*tasks)
         else:
-          #its an external link, could be an advertisement website. ignore it
-          continue
-    results = asyncio.gather(*tasks)
-  else:
-    shutdown()
-
-loop = asyncio.get_event_loop()
-future = asyncio.ensure_future(crawl(base_url, 'Kashmir'))
-loop.run_until_complete(future)
+            self.shutdown()
 
 
-# [^.]* flung [^.]*\.
+if __name__ == '__main__':
+    URL = "http://www.thehindu.com"
+    KEYWORD = 'chennai'
+    loop = asyncio.get_event_loop()
+    crawlObj = WebCrawler(base_url=URL)
+    loop.add_signal_handler(signal.SIGINT, crawlObj.shutdown)
+    future = asyncio.ensure_future(crawlObj.crawl(URL, KEYWORD))
+    loop.run_until_complete(future)
